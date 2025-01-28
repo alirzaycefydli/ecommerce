5a578390cc003f128dedae14f84b7a0be4ebf2da
Ali Rıza Yücefaydalı <alirizayucefaydali@Alis-MacBook-Pro.local>
2025-01-28 14:33:38 +0100

    Cart functions added

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
     /** @use HasFactory<\Database\Factories\CartFactory> */
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $fillable = ['user_id', 'product_id', 'quantity'];
 }
